37b87242ce10c67fb049377ecc1e19fb89a788c8
allenOrpia <118657615+allenOrpia@users.noreply.github.com>
2024-11-19 00:23:24 -0800

    Added auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,34 +2,67 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-
 /* REGISTER USER */
 
-export const register = async(req,res) => {
-    try {
-        // Obtain User Info
-        const {
-            firstName, lastName, email, password, picturePath, friends, location, occupation
-        } = req.body;
-        
-        //Encrypt User Pass
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
+export const register = async (req, res) => {
+  try {
+    // Obtain User Info
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
-        // Create new user with encrypted pass
-        const newUser = new User({
-            firstName, lastName, email, passwordHash,picturePath, friends, location, occupation, viewedProfile: Math.floor(Math.random() * 10000),
-            impresseions: Math.floor(Math.random() * 10000)
-        });
-        
-        // Create and save new User
-        const savedUser = await newUser.save();
+    //Encrypt User Pass
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        // Generate or send the response back to the frontend
-        res.status(201).json(savedUser);
+    // Create new user with encrypted pass
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impresseions: Math.floor(Math.random() * 10000),
+    });
 
+    // Create and save new User
+    const savedUser = await newUser.save();
 
-    } catch (err) {
-        res.status(500).json({ error: err.message})
-    }
-}
+    // Generate or send the response back to the frontend
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGIN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    // If user does exist, compare and check the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials. " });
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "module";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js";
 
 /* Configurations  */
@@ -22,50 +22,51 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({
+app.use(
+  bodyParser.json({
     limit: "30mb",
-    extended:true
-}));
-app.use(bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
     limit: "30mb",
-    extended: true
-}));
+    extended: true,
+  })
+);
 app.use(cors());
 // Set directory of where we keep our assets
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
-
-
-
-
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* File Storage */
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req,file,cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer( { storage });
+const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post('/auth/register', upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
 
-
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6000;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server Port: ${PORT}`)
-    })
-}).catch( (error) => {
-    console.log(`${error} did not connect`)
-});
-
-
-
+      console.log(`Server Port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
