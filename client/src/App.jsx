26051387584b3582f7b977f5b456ad8a51b5b8b8
Allen Orpia <allenorpia17@gmail.com>
2024-12-06 17:11:34 -0800

    added login and register form with authenthication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,33 @@
 /* eslint-disable no-unused-vars */
-import { useState, useMemo } from 'react';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from './scenes/homePage';
-import LoginPage from './scenes/loginPage';
-import ProfilePage from './scenes/profilePage';
-import { useSelector } from 'react-redux';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material';
-
-
-
-
+import { useState, useMemo } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
 
 function App() {
   const mode = useSelector((state) => state.mode);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
-    <div className={`app h-[100vh] w-[100vw] ${mode}`} >
+    <div className={`app h-[100vh] w-[100vw] ${mode}`}>
       <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/profile/:userId" element={<ProfilePage />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
